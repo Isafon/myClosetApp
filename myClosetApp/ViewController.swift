60bea1c3d1826ec13a8c0e2bf190dcb1896ddf5d
Isa Fontana <isfon8@gmail.com>
2022-03-10 13:02:10 -0700

    Initial Commit

---
+++ myClosetApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myClosetApp
+//
+//  Created by isa fontana on 3/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
